a39272a90f69141f7b07764b50d9e6d541ad6bc8
Dzianis Kalesnikau <denkoles3k@gmail.com>
2022-07-14 18:03:41 +0300

    Добавил параметр Онлайн/Оффлайн курс

--- overschool/users/serializers/school_user_offline.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from users.models import SchoolUserOffline
-
-
-class SchoolUserOfflineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = SchoolUserOffline
-        fields = "__all__"
--- overschool/users/urls.py
+++ overschool/users/urls.py
@@ -1,9 +1,8 @@
 from rest_framework import routers
 
-from users.api_views import SchoolUserOfflineViewSet, SchoolUserViewSet
+from users.api_views import SchoolUserViewSet
 
 router = routers.DefaultRouter()
 router.register("users", SchoolUserViewSet, basename="users")
-router.register("users_offline/", SchoolUserOfflineViewSet, basename="users_offline")
 
 urlpatterns = router.urls
