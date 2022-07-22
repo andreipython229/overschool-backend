559de01364c196af30d4e0f41c66a7a6eda3ee55
https://github.com/Vital28 <vipo6118@gmail.com>
2022-07-22 12:58:41 +0300

    add files for upload documentation for licence and confidantiality

---
+++ overschool/users/api_views/confidentDOC_view/views.py
@@ -0,0 +1,9 @@
+from rest_framework import viewsets
+
+from .models import Documents
+from .serializers import UploadSerializer
+
+
+class UploadViewSet(viewsets.ModelViewSet):
+    queryset = Documents.objects.all()
+    serializer_class = UploadSerializer
