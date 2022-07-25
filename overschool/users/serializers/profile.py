18586a8d7af71947ec89b31ed3a8699562b4236f
Maria Burdo <masha.burdo@gmail.com>
2022-07-25 13:12:57 +0300

    добавила создание профиля

--- overschool/users/serializers/profile.py
+++ overschool/users/serializers/profile.py
@@ -1,22 +1,33 @@
 from rest_framework import serializers
 from users.models import Profile
 
+from .user import UserInitialsSerializer
+
 
 class ProfileSerializer(serializers.ModelSerializer):
     """Сериализатор для личного кабинета пользователя"""
 
-    email = serializers.CharField(source="user.email")
-    first_name = serializers.CharField(source="user.first_name")
-    last_name = serializers.CharField(source="user.last_name")
+    user = UserInitialsSerializer()
 
     class Meta:
         model = Profile
         fields = (
-            "email",
-            "first_name",
-            "last_name",
+            "user",
             "phone_number",
             "city",
             "description",
             "sex",
         )
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop("user")
+
+        if user_data:
+            user = instance.user
+            user.first_name = user_data.get("first_name", user.first_name)
+            user.last_name = user_data.get("last_name", user.last_name)
+            user.email = user_data.get("email", user.email)
+
+            user.save()
+
+        return super().update(instance=instance, validated_data=validated_data)
