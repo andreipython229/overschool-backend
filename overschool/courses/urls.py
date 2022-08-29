12b6be68b648cd9e63efbe15cbce221488a65cfd
Dzianis Kalesnikau <denkoles3k@gmail.com>
2022-08-29 11:16:15 +0300

    Добавил URL_header, модель, сериалайзер, эндпоинт

--- overschool/courses/urls.py
+++ overschool/courses/urls.py
@@ -6,15 +6,15 @@ from courses.api_views import (
     LessonViewSet,
     SectionViewSet,
     StudentsGroupViewSet,
-    StudentsGroupCourseViewSet
+    CourseHeaderViewSet,
 )
 
 router = routers.DefaultRouter()
+router.register("course_header", CourseHeaderViewSet, basename="course_header")
 router.register("courses", CourseViewSet, basename="courses")
 router.register("sections", SectionViewSet, basename="sections")
 router.register("lessons", LessonViewSet, basename="lessons")
 router.register("audiofile", AudioFileView, basename="audiofile")
 router.register("students_group", StudentsGroupViewSet, basename="students_group")
-router.register("course_students_group", StudentsGroupCourseViewSet, basename="course_students_group")
 
 urlpatterns = router.urls
