406c71a110b9552716da905184d50a5d2c746a62
https://github.com/Vital28 <vipo6118@gmail.com>
2022-09-13 12:48:43 +0300

    add filters

--- overschool/homeworks/paginators/user_homework.py
+++ overschool/homeworks/paginators/user_homework.py
@@ -5,4 +5,4 @@ class UserHomeworkPagination(pagination.PageNumberPagination):
     page_size = 25
     page_size_query_param = 'limit'
     max_page_size = 25
-    page_query_param = 'p'
+    page_query_param = 'page'
