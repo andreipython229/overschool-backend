c346d5bf86f6a9702e3df97f3e6d1c09c4f8ff05
ArtyrZh <artyr.zhumabekau@innowise-group.com>
2022-09-23 15:17:15 +0400

    SYVD - 104 * Repair views

--- overschool/courses/api_views/course.py
+++ overschool/courses/api_views/course.py
@@ -8,6 +8,7 @@ from rest_framework import generics, permissions, status, viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
 from users.models import User
+from datetime import datetime
 
 
 class CourseViewSet(LoggingMixin, WithHeadersViewSet, viewsets.ModelViewSet):
@@ -59,30 +60,32 @@ class UsersCourse(LoggingMixin, WithHeadersViewSet, generics.ListAPIView):
     queryset = User.objects.all()
     permission_classes = [permissions.DjangoModelPermissions]
     pagination_class = UserHomeworkPagination
-    serializer_class = CourseStudentsSerializer
 
     def list(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            queryset = self.get_queryset(**serializer.data)
+        try:
+            queryset = self.get_queryset(course=request.GET['course_id'])
             paginator = self.pagination_class()
             data = paginator.paginate_queryset(request=request, queryset=queryset)
             return paginator.get_paginated_response(data=data)
-        else:
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except KeyError as e:
+            return Response(data={"status": "Error", "message": "No course_id"},
+                            status=status.HTTP_400_BAD_REQUEST)
 
     def get_queryset(self, *args, **kwargs):
-        queryset = StudentsGroup.objects.filter(course_id=1)
-        data = queryset.values(
-            email=F("students__email"),
-            student_name=F("students__first_name"),
-            student=F("students__user_id"),
-            group=F("group_id"),
-        ).annotate(
+        queryset = StudentsGroup.objects.filter(course_id=kwargs['course'])
+        data = queryset.values(course=F("course_id"),
+                               email=F("students__email"),
+                               student_name=F("students__first_name"),
+                               student=F("students__id"),
+                               group=F("group_id"),
+                               last_active=F("students__date_joined"),
+                               update_date=F("students__date_joined"),
+                               ending_date=F("students__date_joined")
+                               ).annotate(
             mark_sum=Sum("students__user_homeworks__mark"),
             average_mark=Avg("students__user_homeworks__mark"),
             progress=(F("students__user_progresses__lesson__order") * 100)
-            / Count("course_id__sections__lessons__lesson_id"),
+                     / Count("course_id__sections__lessons__lesson_id"),
         )
         for row in data:
             mark_sum = (
@@ -90,5 +93,30 @@ class UsersCourse(LoggingMixin, WithHeadersViewSet, generics.ListAPIView):
                 .values("user")
                 .aggregate(mark_sum=Sum("success_percent"))["mark_sum"]
             )
-            row["mark_sum"] += mark_sum // 10 if mark_sum else 0
+            row["mark_sum"] += mark_sum // 10 if bool(mark_sum) else 0
         return data
+
+
+class CourseUsersByMonthView(LoggingMixin, WithHeadersViewSet, generics.ListAPIView):
+    queryset = User.objects.all()
+    permission_classes = [permissions.DjangoModelPermissions]
+
+    def list(self, request, *args, **kwargs):
+        try:
+            queryset = self.get_queryset(
+                month_number=request.GET['month_number'] if 'month_number' in request.GET else datetime.now().month,
+                course_id=request.GET['course_id'])
+            return Response(queryset, status=status.HTTP_200_OK)
+        except KeyError as e:
+            return Response(data={"status": "Error", "message": "No course_id"}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_queryset(self, *args, **kwargs):
+        queryset = StudentsGroup.objects.filter(course_id=kwargs['course_id'],
+                                                students__date_joined__month=kwargs["month_number"])
+        datas = queryset.values(course=F("course_id")).annotate(students_sum=Count("students__id"))
+        for data in datas:
+            data["graphic_data"] = queryset.values(course=F("course_id"),
+                                                   date=F("students__date_joined__day")).annotate(
+                students_sum=Count("students__id")
+            )
+        return datas
