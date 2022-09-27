271720aee482c78234073d5ed822d5c6eee49d49
mashaBurdo <masha.burdo@gmail.com>
2022-09-27 16:29:58 +0300

    add url

--- overschool/courses/api_views/__init__.py
+++ overschool/courses/api_views/__init__.py
@@ -5,3 +5,4 @@ from .section import SectionViewSet
 from .audio_file import AudioFileView
 from .students_group import StudentsGroupViewSet, GroupUsersByMonthView, UsersGroup
 from .school_header import SchoolHeaderViewSet
+from .students_table_info import StudentsTableInfoViewSet
