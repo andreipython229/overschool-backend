e70438bb20c1b505541c7d611b0588b6c401375d
Dzianis Kalesnikau <denkoles3k@gmail.com>
2022-09-30 16:53:36 +0300

    Починили и объединили вьюхи через action методы, добавили описание в свагере

--- overschool/courses/urls.py
+++ overschool/courses/urls.py
@@ -2,16 +2,11 @@ from rest_framework import routers
 
 from courses.api_views import (
     CourseViewSet,
-    CourseDataSet,
     AudioFileView,
     LessonViewSet,
     SectionViewSet,
     StudentsGroupViewSet,
     SchoolHeaderViewSet,
-    UsersCourse,
-    GroupUsersByMonthView,
-    CourseUsersByMonthView,
-    UsersGroup
 )
 
 router = routers.DefaultRouter()
@@ -21,10 +16,5 @@ router.register("sections", SectionViewSet, basename="sections")
 router.register("lessons", LessonViewSet, basename="lessons")
 router.register("audiofile", AudioFileView, basename="audiofile")
 router.register("students_group", StudentsGroupViewSet, basename="students_group")
-router.register("course_stat", UsersCourse, basename="course_stat")
-router.register("user_count_by_month_group", GroupUsersByMonthView, basename="user_count_by_month_group")
-router.register("user_count_by_month_course", CourseUsersByMonthView, basename="user_count_by_month_course")
-router.register("group_stat", UsersGroup, basename="group_stat")
-router.register("courses_data", CourseDataSet, basename="courses_data")
 
 urlpatterns = router.urls
