524c65b511151e8b94fdec53489540c29dea2506
alexsenokosov <712020:aa1c0711-8013-4e33-9f9e-104d50ef4714@bots.bitbucket.org>
2023-05-21 11:23:17 +0300

    fix config.env to .env in settings

--- overschool/overschool/settings.py
+++ overschool/overschool/settings.py
@@ -19,7 +19,7 @@ from .sentry import *
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 env = Env(DEBUG=(bool, False))
-Env.read_env(str(BASE_DIR / "config.env"))
+Env.read_env(str(BASE_DIR / ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
