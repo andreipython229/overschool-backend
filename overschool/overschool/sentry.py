253221d581b3843db509a93226e1467609212b45
https://github.com/Vital28 <vipo6118@gmail.com>
2023-05-22 17:01:36 +0300

    обновил настройки для Сентри , добавил логгер и менеджеров в сеттинги,подключил сентри

--- overschool/overschool/sentry.py
+++ overschool/overschool/sentry.py
@@ -1,17 +1,18 @@
+import datetime
 import logging
+import time
+
 import sentry_sdk
 from sentry_sdk.integrations.django import DjangoIntegration
 from sentry_sdk.integrations.logging import SentryHandler
-import time
-import datetime
+
 SENTRY_INIT_OPTIONS = {
     'dsn': 'https://84b3a15795cd47f5a01a552151dabc06@o4505167864463360.ingest.sentry.io/4505173034270720',
     'integrations': [
         DjangoIntegration(
             transaction_style='url',
             middleware_spans=True,
-            signals_spans=False,
-            cache_spans=False,
+
         ),
     ],
     'send_default_pii': True,
@@ -29,7 +30,7 @@ handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)
 logger.addHandler(handler)
 
 message = 'Сервер успешно запущен!'
-timestamp = time.time()+10800
+timestamp = time.time() + 10800
 formatted_time = datetime.datetime.fromtimestamp(timestamp).strftime('%d-%m-%Y -> %H:%M')
 formatted_message = '{} (время записи: {})'.format(message, formatted_time)
 logger.info(formatted_message)
