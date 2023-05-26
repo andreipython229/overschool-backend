58eccb27b5fcf68ebbb4f171d6f098707eeae1b0
https://github.com/Vital28 <vipo6118@gmail.com>
2023-05-26 21:11:04 +0300

    permissions chat

--- overschool/chats/views.py
+++ overschool/chats/views.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.models import User
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import permissions
 from rest_framework import status
 from rest_framework.parsers import MultiPartParser
 from rest_framework.response import Response
 from rest_framework.views import APIView
-
 from .constants import CustomResponses
 from .models import Chat, Message, UserChat
 from .request_params import ChatParams, UserParams
