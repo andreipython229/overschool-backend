674872b78f6bd5a3bc8940eeed3176e0787d21a9
Denis Sturchenko <developden@gmail.com>
2023-06-14 19:41:26 +0300

    fix

--- overschool/courses/urls.py
+++ overschool/courses/urls.py
@@ -12,7 +12,7 @@ router.register("courses", CourseViewSet, basename="courses")
 router.register("sections", SectionViewSet, basename="sections")
 router.register("lessons", LessonViewSet, basename="lessons")
 router.register("students_group", StudentsGroupViewSet, basename="students_group")
-router.register("students_group_settings", StudentsGroupSettingsViewSet, basename="students_group")
+router.register("students_group_settings", StudentsGroupSettingsViewSet, basename="students_group_settings")
 router.register(
     "students_table_info", StudentsTableInfoViewSet, basename="students_table_info"
 )
