d3ffa38e250c660a5a6dffcef84d8b7725637bc7
Kolesnikov Denis <denkoles3k@gmail.com>
2023-06-17 17:19:52 +0300

    Добавил миксин

--- overschool/common_services/mixins/__init__.py
+++ overschool/common_services/mixins/__init__.py
@@ -1,6 +1,5 @@
 from .author_mixin import AuthorMixin
 from .logging_mixins import LoggingErrorsMixin, LoggingMixin
 from .order_mixin import OrderMixin
-from .school_mixin import SchoolMixin
 from .timestamp_mixin import TimeStampMixin
 from .with_headers_mixin import WithHeadersViewSet
