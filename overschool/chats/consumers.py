17b6ae004033b3937a6b697116d18a1b36569a70
https://github.com/Vital28 <vipo6118@gmail.com>
2023-07-13 14:21:27 +0300

    Чаты полностью готовы

--- overschool/chats/consumers.py
+++ overschool/chats/consumers.py
@@ -1,11 +1,14 @@
 import json
 
+import jwt
+from asgiref.sync import sync_to_async
 from channels.db import database_sync_to_async
 from channels.exceptions import DenyConnection
 from channels.generic.websocket import AsyncWebsocketConsumer
 
 from .constants import CustomResponses
 from .models import Chat, UserChat, Message
+from users.models import User
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
@@ -36,18 +39,49 @@ class ChatConsumer(AsyncWebsocketConsumer):
     def set_room_group_name(self):
         self.room_group_name = f'chat_{self.chat_uuid}'
 
+    async def get_user_id_from_token(self, token):
+        decoded_token = jwt.decode(token, options={"verify_signature": False})
+        return decoded_token["sub"]
+
+    def get_token_from_headers(self):
+        headers = self.scope["headers"]
+        token = None
+        for head in headers:
+            if head[0] == b"cookie":
+                cookies = head[1].decode("utf-8")
+                cookies_list = cookies.split(";")
+                for cookie in cookies_list:
+                    if "access_token" in cookie:
+                        token = cookie.replace("access_token=", "")
+        if token is None:
+            raise DenyConnection(CustomResponses.invalid_cookie)
+        return token
+
     async def connect(self):
         self.chat_uuid = self.scope['url_route']['kwargs']['room_name']
         self.chat = await self.is_chat_exist(self.chat_uuid)
         if self.chat is False:
             raise DenyConnection(CustomResponses.chat_not_exist)
 
-        self.user = self.scope['user']
+        self.token = self.get_token_from_headers()
+        print(self.token)
+        user_id = await self.get_user_id_from_token(self.token)
+        if user_id is None:
+            raise DenyConnection(CustomResponses.invalid_cookie)
+
+        try:
+            self.user = await sync_to_async(User.objects.get)(id=user_id)
+        except User.DoesNotExist:
+            raise DenyConnection(CustomResponses.invalid_cookie)
+        print(self.user)
+        if self.user is None:
+            raise DenyConnection(CustomResponses.invalid_cookie)
+
         user_is_chat_participant = await self.is_chat_participant(self.user, self.chat)
         if user_is_chat_participant is False:
-            # raise DenyConnection(CustomResponses.no_permission)
+            raise DenyConnection(CustomResponses.no_permission)
 
-            self.set_room_group_name()
+        self.set_room_group_name()
 
         await self.channel_layer.group_add(
             self.room_group_name,
