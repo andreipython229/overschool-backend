1cf422a3efae5ed0939951414a0fbc9fa9b3f85b
Kolesnikov Denis <denkoles3k@gmail.com>
2023-07-16 14:12:18 +0300

    Fix

--- overschool/overschool/asgi.py
+++ overschool/overschool/asgi.py
@@ -5,7 +5,7 @@ import time
 
 import chats.routing
 import sentry_sdk
-from channels.auth import AuthMiddlewareStack
+from channels.middleware import BaseMiddleware
 from channels.routing import ProtocolTypeRouter, URLRouter
 from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
@@ -14,9 +14,9 @@ from sentry_sdk.integrations.logging import SentryHandler
 
 # Настройки Sentry
 SENTRY_INIT_OPTIONS = {
-    'dsn': 'https://84b3a15795cd47f5a01a552151dabc06@o4505167864463360.ingest.sentry.io/4505173034270720',
-    'integrations': [DjangoIntegration()],
-    'send_default_pii': True,
+    "dsn": "https://84b3a15795cd47f5a01a552151dabc06@o4505167864463360.ingest.sentry.io/4505173034270720",
+    "integrations": [DjangoIntegration()],
+    "send_default_pii": True,
 }
 sentry_sdk.init(**SENTRY_INIT_OPTIONS)
 
@@ -25,25 +25,36 @@ logger.setLevel(logging.INFO)
 
 handler = SentryHandler()
 handler.setLevel(logging.INFO)
-handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+handler.setFormatter(logging.Formatter("%(asctime)s - %(levelname)s - %(message)s"))
 
 logger.addHandler(handler)
 
-message = 'Сервер успешно запущен!'
+message = "Сервер успешно запущен!"
 timestamp = time.time() + 10800
-formatted_time = datetime.datetime.fromtimestamp(timestamp).strftime('%d-%m-%Y -> %H:%M')
-formatted_message = '{} (время записи: {})'.format(message, formatted_time)
+formatted_time = datetime.datetime.fromtimestamp(timestamp).strftime(
+    "%d-%m-%Y -> %H:%M"
+)
+formatted_message = "{} (время записи: {})".format(message, formatted_time)
 logger.info(formatted_message)
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "overschool.settings")
 django_asgi_app = get_asgi_application()
 
-application = ProtocolTypeRouter({
-    "http": django_asgi_app,
-    "websocket": AllowedHostsOriginValidator(
-        AuthMiddlewareStack(
-            URLRouter(
-                chats.routing.websocket_urlpatterns
-            )
-        )
-    ),
-})
+
+class CorsHeadersMiddleware(BaseMiddleware):
+    async def __call__(self, scope, receive, send):
+        response = await super().__call__(scope, receive, send)
+        response["access-control-allow-credentials"] = "true"
+        response["Access-Control-Allow-Origin"] = "*"
+        response["Access-Control-Allow-Methods"] = "*"
+        return response
+
+
+application = ProtocolTypeRouter(
+    {
+        "http": django_asgi_app,
+        "websocket": AllowedHostsOriginValidator(
+            URLRouter(chats.routing.websocket_urlpatterns)
+        ),
+    }
+)
+application = CorsHeadersMiddleware(application)
