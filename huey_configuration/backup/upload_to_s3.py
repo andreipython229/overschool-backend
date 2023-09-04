7d9ccdc77216d4c7b5ff983256c0a29187466150
Kolesnikov Denis <denkoles3k@gmail.com>
2023-09-04 12:04:18 +0300

    fix order

--- huey_configuration/backup/upload_to_s3.py
+++ huey_configuration/backup/upload_to_s3.py
@@ -5,38 +5,51 @@ import zipfile
 
 from config.config import CONTAINER_NAME
 from config.selectel_client import SelectelClient
+from loguru import logger
 
 selectel_client = SelectelClient()
 
 
 def compress_and_upload_backup(backup_path, db_name):
-    timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    zip_file_path = f"{db_name}_{timestamp}.zip"
+    try:
+        timestamp = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        zip_file_path = f"{db_name}_{timestamp}.zip"
 
-    # Сжимаем бэкап
-    with zipfile.ZipFile(zip_file_path, "w") as zip_file:
-        zip_file.write(backup_path)
+        # Сжимаем бэкап
+        with zipfile.ZipFile(zip_file_path, "w") as zip_file:
+            zip_file.write(backup_path)
 
-    # Загружаем в Selectel
-    with open(zip_file_path, "rb") as f:
-        selectel_client.upload_to_selectel(
-            f"{CONTAINER_NAME}/{db_name}/{zip_file_path}",
-            f.read()
-        )
-
-    # Удаляем локальные файлы
-    os.remove(backup_path)
-    os.remove(zip_file_path)
+        # Загружаем в Selectel
+        with open(zip_file_path, "rb") as f:
+            selectel_client.upload_to_selectel(
+                f"{CONTAINER_NAME}/{db_name}/{zip_file_path}",
+                f.read()
+            )
+            logger.info(f"Successfully compressed and uploaded {db} database backup")
+    except Exception as e:
+        logger.error(f"Error compressing and uploading {db} database backup: {e}")
+    finally:
+        # Удаляем локальные файлы
+        os.remove(backup_path)
+        os.remove(zip_file_path)
 
 
 def delete_old_backups(db_name, max_backups=7):
-    # Получаем список файлов бэкапов
-    files = selectel_client.get_folder_files(f"{CONTAINER_NAME}/{db_name}/")
+    try:
+        # Получаем список файлов бэкапов
+        files = selectel_client.get_folder_files(f"{CONTAINER_NAME}/{db_name}/")
+    except Exception as e:
+        logger.debug(
+            f"{e} The errors were caused by trying to connect to Selectel Cloud Storage"
+        )
 
     # Сортируем по дате изменения
     sorted_files = sorted(files, key=lambda f: f['last_modified'], reverse=True)
+    num_backups = len(sorted_files)
+    logger.debug(f"Has {num_backups} backups")
 
     # Удаляем старые бэкапы, если их больше максимального лимита
     if len(sorted_files) > max_backups:
+        logger.debug(f"Deleting old backups")
         for f in sorted_files[max_backups:]:
             selectel_client.remove_from_selectel(f)
--- overschool/common_services/mixins/order_mixin.py
+++ overschool/common_services/mixins/order_mixin.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class OrderMixin(models.Model):
-    order = models.IntegerField(verbose_name="Порядок")
+    order = models.AutoField(verbose_name="Порядок")
 
     class Meta:
         ordering = [models.F("order").desc(nulls_last=True)]
