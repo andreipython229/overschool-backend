fd8b3be4850fb9fd1a088b2e246eff1a9a732b00
Kolesnikov Denis <denkoles3k@gmail.com>
2023-09-04 12:34:10 +0300

    fix order

--- overschool/courses/serializers/section.py
+++ overschool/courses/serializers/section.py
@@ -8,7 +8,12 @@ class SectionSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Section
-        fields = ("section_id", "course", "name", "lessons")
+        fields = ("order", "section_id", "course", "name", "lessons")
+
+    def create(self, validated_data):
+        # Удаляем order из validated_data, чтобы не пытаться передавать его явно
+        validated_data.pop('order', None)
+        return super().create(validated_data)
 
     def to_representation(self, instance):
         data = super().to_representation(instance)
