937ea0b1e4c4a60e8560b2bd84e19f56aeb3c300
Kolesnikov Denis <denkoles3k@gmail.com>
2023-09-14 14:43:49 +0300

    fix

--- overschool/common_services/mixins/order_mixin.py
+++ overschool/common_services/mixins/order_mixin.py
@@ -1,14 +1,15 @@
 from django.db import models
 
-# def generate_order(model):
-#     # Ваша логика для определения порядка
-#     # Например, получение максимального порядка из базы данных для данной модели и увеличение его на 1
-#
-#     max_order = model.objects.all().aggregate(models.Max('order'))['order__max']
-#     if max_order is not None:
-#         return max_order + 1
-#     else:
-#         return 1
+
+def generate_order(model):
+    # Ваша логика для определения порядка
+    # Например, получение максимального порядка из базы данных для данной модели и увеличение его на 1
+
+    max_order = model.objects.all().aggregate(models.Max('order'))['order__max']
+    if max_order is not None:
+        return max_order + 1
+    else:
+        return 1
 
 
 class OrderMixin(models.Model):
