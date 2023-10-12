3e31a89f0359e10f375cc3de8655c9125a567029
Kolesnikov Denis <denkoles3k@gmail.com>
2023-10-12 18:01:48 +0300

    Настроил апи приёма ответа от bepaid

--- overschool/utils/notification.py
+++ overschool/utils/notification.py
@@ -19,6 +19,8 @@ class PaymentNotificationView(LoggingMixin, WithHeadersViewSet, APIView):
     def post(self, request):
         signature = request.headers.get("Content-Signature")
         signature_other = request.headers.get("Signature")
+        signature_3 = request.META.get("Content-Signature")
+        signature_4 = request.META.get("Signature")
         shop_public_key = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAybT85/HNo9IXHRBoA32wgrnvUZaWK2RcNXoKZju2hRM+3B9KR5qD1aDXnKN2VzLe47XLhKaiiwbAAToThBAxVHhKh25PlerP9iAKLYDypihrbuJEq1QHQzPFRjIliE66NXKh6KGB0wv3ZCakaqAJgx3GH9fZKU5QdmSlYIwyfOI+z01T4cLmdDPOz/NAsgFBU0RwvPJd9aXXb7O8fm8MIxahksvU337BUSZjBbGUKWNIJ+6t4dLXQqv4o9axejRMkGmSY3Puq06t4nBqCgXdgwM3ovk5L6KjxaIw/Vc0edbf6bcLpj/GpML0k49GAisnn4jJaTiW2LzI2up8pj5uQwIDAQAB"
         body_bytes = bytes(request.body)
         digest = (
@@ -26,7 +28,7 @@ class PaymentNotificationView(LoggingMixin, WithHeadersViewSet, APIView):
             .hexdigest()
             .lower()
         )
-        if digest == signature or digest == signature_other:
+        if digest == signature:
             notification = request.data
             print(notification)
 
@@ -40,7 +42,7 @@ class PaymentNotificationView(LoggingMixin, WithHeadersViewSet, APIView):
             print(digest)
             return Response(
                 {
-                    "error": f"Invalid Signature {digest}!={signature}!={signature_other}, {n}, {a}"
+                    "error": f"Invalid Signature {digest}!={signature}!={signature_other}!={signature_3}!={signature_4}, {n}, {a}"
                 },
                 status=status.HTTP_400_BAD_REQUEST,
             )
