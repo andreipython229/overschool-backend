cce4be75ecd31a5fd7b87e736440c908616cce2e
Kolesnikov Denis <denkoles3k@gmail.com>
2023-10-13 19:30:55 +0300

    fix

--- overschool/users/services/authentication.py
+++ overschool/users/services/authentication.py
@@ -32,62 +32,30 @@ class CustomAuthentication(BaseAuthentication):
 
         except InvalidTokenError:
             try:
-                development_mode_header = request.META.get("HTTP_X_DEVELOPMENT_MODE")
-                if development_mode_header and development_mode_header == "false":
-                    SESSION_COOKIE_DOMAIN = settings.SESSION_COOKIE_DOMAIN
-                    # Ваш код для декодирования refresh_token и получения пользователя
-                    payload = decode(
-                        refresh_token,
-                        settings.JWT_REFRESH_SECRET_KEY,
-                        algorithms=[settings.ALGORITHM],
-                    )
-                    user = User.objects.get(pk=payload.get("sub"))
-                    request.user = user
+                # Ваш код для декодирования refresh_token и получения пользователя
+                payload = decode(
+                    refresh_token,
+                    settings.JWT_REFRESH_SECRET_KEY,
+                    algorithms=[settings.ALGORITHM],
+                )
+                user = User.objects.get(pk=payload.get("sub"))
+                request.user = user
 
-                    # Обновите access_token
-                    new_access_token = jwt_handler.create_access_token(user.id)
-                    response = HttpResponseRedirect(request.path_info)
-                    response.delete_cookie(
-                        settings.ACCESS, domain=SESSION_COOKIE_DOMAIN
-                    )
-                    response.delete_cookie(
-                        settings.REFRESH, domain=SESSION_COOKIE_DOMAIN
-                    )
-                    response.set_cookie(
-                        key=settings.ACCESS,
-                        value=new_access_token,
-                        max_age=settings.COOKIE_EXPIRE_SECONDS,
-                        expires=settings.COOKIE_EXPIRE_SECONDS,
-                        httponly=True,
-                        samesite=None,
-                        secure=False,
-                        domain=SESSION_COOKIE_DOMAIN,
-                    )
-                    return user, new_access_token
-                else:
-                    # Ваш код для декодирования refresh_token и получения пользователя
-                    payload = decode(
-                        refresh_token,
-                        settings.JWT_REFRESH_SECRET_KEY,
-                        algorithms=[settings.ALGORITHM],
-                    )
-                    user = User.objects.get(pk=payload.get("sub"))
-                    request.user = user
+                # Обновите access_token
+                new_access_token = jwt_handler.create_access_token(user.id)
+                response = HttpResponseRedirect(request.path_info)
+                response.delete_cookie(settings.ACCESS)
+                response.delete_cookie(settings.REFRESH)
+                response.set_cookie(
+                    key=settings.ACCESS,
+                    value=new_access_token,
+                    max_age=settings.COOKIE_EXPIRE_SECONDS,
+                    expires=settings.COOKIE_EXPIRE_SECONDS,
+                    httponly=True,
+                    samesite=None,
+                    secure=False,
+                )
+                return user, new_access_token
 
-                    # Обновите access_token
-                    new_access_token = jwt_handler.create_access_token(user.id)
-                    response = HttpResponseRedirect(request.path_info)
-                    response.delete_cookie(settings.ACCESS)
-                    response.delete_cookie(settings.REFRESH)
-                    response.set_cookie(
-                        key=settings.ACCESS,
-                        value=new_access_token,
-                        max_age=settings.COOKIE_EXPIRE_SECONDS,
-                        expires=settings.COOKIE_EXPIRE_SECONDS,
-                        httponly=True,
-                        samesite=None,
-                        secure=False,
-                    )
-                    return user, new_access_token
             except:
                 return None
--- overschool/users/services/middleware.py
+++ overschool/users/services/middleware.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.contrib.auth import get_user_model
+from django.http import HttpResponseRedirect
 from django.utils.deprecation import MiddlewareMixin
 from jwt import InvalidTokenError, decode
 
@@ -11,7 +12,7 @@ jwt_handler = JWTHandler()
 
 
 class AuthOptionalMiddleware(MiddlewareMixin):
-    def process_view(self, request):
+    def process_view(self, request, view_func, view_args, view_kwargs):
         access_token = request.COOKIES.get(settings.ACCESS)
         refresh_token = request.COOKIES.get(settings.REFRESH)
         if not (access_token and refresh_token):
@@ -26,4 +27,30 @@ class AuthOptionalMiddleware(MiddlewareMixin):
             user = User.objects.get(pk=payload.get("sub"))
             request.user = user
         except InvalidTokenError:
-            return None
+            try:
+                payload = decode(
+                    refresh_token,
+                    settings.JWT_REFRESH_SECRET_KEY,
+                    algorithms=[settings.ALGORITHM],
+                )
+                user = User.objects.get(pk=payload.get("sub"))
+                request.user = user
+                # Refresh access token here
+                new_access_token = jwt_handler.create_access_token(user.id)
+                request.COOKIES[settings.ACCESS] = new_access_token
+                response = HttpResponseRedirect(request.path_info)
+                response.delete_cookie(settings.ACCESS)
+                response.delete_cookie(settings.REFRESH)
+                response.set_cookie(
+                    key=settings.ACCESS,
+                    value=new_access_token,
+                    max_age=settings.COOKIE_EXPIRE_SECONDS,
+                    expires=settings.COOKIE_EXPIRE_SECONDS,
+                    httponly=True,
+                    samesite=None,
+                    secure=False,
+                )
+                return response
+            except:
+                request.user = None
+        return None
