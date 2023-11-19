2f14b619a63372edbee99b04741d44a808ed8e6d
https://github.com/Vital28 <vipo6118@gmail.com>
2023-11-19 16:42:01 +0300

    тариф

--- overschool/users/api_views/tariff_school_owner.py
+++ overschool/users/api_views/tariff_school_owner.py
@@ -38,42 +38,41 @@ class TariffSchoolOwner(WithHeadersViewSet, SchoolMixin, APIView):
         try:
             school = School.objects.get(name=school_name, owner=user)
         except School.DoesNotExist:
-            return Response([])
+            return Response({"error": "School not found"})
 
-        if school.tariff.name == TariffPlan.INTERN.value:
-            data = {"tariff_name": school.tariff.name, "days_left": None, "students": 0, "staff": 0}
+        if school.tariff:
+
+            if school.purchased_tariff_end_date:
+                days_left = (school.purchased_tariff_end_date - timezone.now()).days
+            elif school.trial_end_date:
+                days_left = (school.trial_end_date - timezone.now()).days
+            else:
+                days_left = None
+
+            users = User.objects.filter(groups__school=school)
+
+            students = 0
+            staff = 0
+
+            for user in users:
+                user_group = UserGroup.objects.filter(user=user, school=school).first()
+                if user_group:
+                    role_name = user_group.group.name
+                    if role_name == "Student":
+                        students += 1
+                    else:
+                        staff += 1
+
+            courses_for_school = Course.objects.filter(school=school)
+            number_of_courses = courses_for_school.count()
+
+            data = {
+                "tariff_name": school.tariff.name,
+                "days_left": days_left,
+                "students": students,
+                "staff": staff,
+                "tariff": school.tariff.id,
+                "number_of_courses": number_of_courses
+            }
+
             return Response(data)
-
-        if school.purchased_tariff_end_date:
-            days_left = (school.purchased_tariff_end_date - timezone.now()).days
-        elif school.trial_end_date:
-            days_left = (school.trial_end_date - timezone.now()).days
-        else:
-            days_left = None
-
-        users = User.objects.filter(groups__school=school)
-
-        students = 0
-        staff = 0
-
-        for user in users:
-            user_group = UserGroup.objects.filter(user=user, school=school).first()
-            if user_group:
-                role_name = user_group.group.name
-                if role_name == "Student":
-                    students += 1
-                else:
-                    staff += 1
-        courses_for_school = Course.objects.filter(school=school)
-        number_of_courses = courses_for_school.count()
-
-        data = {
-            "tariff_name": school.tariff.name,
-            "days_left": days_left,
-            "students": students,
-            "staff": staff,
-            "tariff": school.tariff.id,
-            "number_of_courses": number_of_courses
-        }
-
-        return Response(data)
