66eb96149080891eb2a8bd5a0437085f657e16e8
Kolesnikov Denis <denkoles3k@gmail.com>
2023-12-25 18:08:38 +0300

    fix profile

--- overschool/users/serializers/profile.py
+++ overschool/users/serializers/profile.py
@@ -12,6 +12,7 @@ class ProfileSerializer(serializers.ModelSerializer):
             "username",
             "first_name",
             "last_name",
+            "patronymic",
             "email",
             "phone_number",
         ]
@@ -41,6 +42,7 @@ class UserProfileSerializer(serializers.ModelSerializer):
             user = instance.user
             user.first_name = user_data.get("first_name", user.first_name)
             user.last_name = user_data.get("last_name", user.last_name)
+            user.patronymic = user_data.get("patronymic", user.patronymic)
             user.email = user_data.get("email", user.email)
             user.phone_number = user_data.get("phone_number", user.phone_number)
 
