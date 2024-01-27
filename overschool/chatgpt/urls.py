0bb53bb9076b8e6d67ee9a10989673a857a2ac1e
Dmitry Kulaga <dima.kulaga5@gmail.com>
2024-01-27 13:29:44 +0300

    View для удаления пустых чатов

--- overschool/chatgpt/urls.py
+++ overschool/chatgpt/urls.py
@@ -13,12 +13,18 @@ urlpatterns = [
         chatgpt.views.LastTenMessages.as_view(),
         name="latest_messages"
     ),
-    path('create_chat/',
+    path(
+        'create_chat/',
          chatgpt.views.CreateChatView.as_view(),
          name='create_chat'
     ),
     path('latest_chats/<int:user_id>/',
          chatgpt.views.LastTenChats.as_view(),
          name='latest_chats'
+    ),
+    path(
+        'delete_chats/<int:user_id>/',
+        chatgpt.views.DeleteChatsView.as_view(),
+        name='delete_chats'
     ),
 ]
