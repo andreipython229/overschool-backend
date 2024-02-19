bb42bfd18c915b58b77df44c849475dd0f1d51e3
boa72 <olya.borisevich29@gmail.com>
2024-02-19 21:42:50 +0300

    Изменен формат ответа в register_user; добавление учеников в группы в access_distribution

--- overschool/users/api_views/register.py
+++ overschool/users/api_views/register.py
@@ -75,8 +75,9 @@ class SendPasswordView(LoggingMixin, WithHeadersViewSet, generics.GenericAPIView
         last_name = serializer.validated_data.get("last_name")
         patronymic = serializer.validated_data.get("patronymic")
 
-        if User.objects.filter(email=email).exists():
-            return Response("User already exists", status=status.HTTP_200_OK)
+        existing_user = User.objects.filter(email=email).first()
+        if existing_user:
+            return Response({"user_id": existing_user.id}, status=status.HTTP_200_OK)
 
         password = generate_random_password()
 
