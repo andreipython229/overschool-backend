655f4afc79b7131a37035ba7f90ef23082a29df9
Kolesnikov Denis <denkoles3k@gmail.com>
2024-03-19 12:37:30 +0300

    fix

--- overschool/courses/serializers/course_appeals.py
+++ overschool/courses/serializers/course_appeals.py
@@ -3,6 +3,22 @@ from rest_framework import serializers
 
 
 class CourseAppealsSerializer(serializers.ModelSerializer):
+    course_name = serializers.SerializerMethodField()
+
     class Meta:
         model = CourseAppeals
-        fields = "__all__"
+        fields = [
+            "id",
+            "course",
+            "course_name",
+            "name",
+            "email",
+            "phone",
+            "message",
+            "is_read",
+            "created_at",
+            "updated_at",
+        ]
+
+    def get_course_name(self, obj):
+        return obj.course.name
