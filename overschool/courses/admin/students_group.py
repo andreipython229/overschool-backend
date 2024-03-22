9218b9650d84a639905220144040c6788ac07f00
Kolesnikov Denis <denkoles3k@gmail.com>
2024-03-22 09:30:29 +0300

    fix

--- overschool/courses/admin/students_group.py
+++ overschool/courses/admin/students_group.py
@@ -1,8 +1,22 @@
+from courses.models import StudentsGroup, StudentsHistory
 from django.contrib import admin
-
-from courses.models import StudentsGroup
 
 
 @admin.register(StudentsGroup)
 class StudentsGroupAdmin(admin.ModelAdmin):
     pass
+
+
+@admin.register(StudentsHistory)
+class StudentsHistoryAdmin(admin.ModelAdmin):
+    list_display = [
+        "user",
+        "students_group",
+        "date_added",
+        "date_removed",
+        "is_deleted",
+    ]
+    list_filter = ["date_added", "date_removed", "is_deleted"]
+    search_fields = ["user__username", "students_group__name"]
+    date_hierarchy = "date_added"
+    readonly_fields = ["date_added", "date_removed"]
