bd62f35aa72de0a414f1df819b8c0c1f241d23df
Kolesnikov Denis <denkoles3k@gmail.com>
2024-03-31 11:43:00 +0300

    Добавил фильтра и сортировки для заявок

--- overschool/courses/admin/course.py
+++ overschool/courses/admin/course.py
@@ -1,6 +1,6 @@
 from courses.models import Course, CourseAppeals
 from django.contrib import admin
-
+from rangefilter.filters import DateRangeFilter
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
@@ -18,4 +18,16 @@ class CourseAdmin(admin.ModelAdmin):
 
 @admin.register(CourseAppeals)
 class CourseAppealsAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('get_school_name', 'course','name', 'email', 'phone',  'created_at')
+    list_filter = (('created_at', DateRangeFilter),'course__school__name', 'email', 'phone', 'name', 'course')
+    search_fields = ('name', 'email', 'phone', 'course__name', 'course__school__name')
+    ordering = ('-created_at',)
+
+    def get_queryset(self, request):
+        qs = super().get_queryset(request)
+        return qs.select_related('course__school')
+
+    def get_school_name(self, obj):
+        return obj.course.school.name
+
+    get_school_name.short_description = 'Школа'
