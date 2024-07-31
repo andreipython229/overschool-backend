e68ad09d0f4d7d10f1299939f86a697659b2554a
Kolesnikov Denis <denkoles3k@gmail.com>
2024-07-31 11:56:05 +0300

    fix

--- overschool/users/api_views/access_distribution.py
+++ overschool/users/api_views/access_distribution.py
@@ -14,12 +14,12 @@ from django.contrib.auth import get_user_model
 from django.contrib.auth.models import Group
 from django.http import HttpResponse
 from django.template.loader import render_to_string
-from drf_yasg.utils import swagger_auto_schema
 from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import generics, permissions, status
-from rest_framework.response import Response
 from rest_framework.decorators import action
 from rest_framework.parsers import MultiPartParser
+from rest_framework.response import Response
 from schools.models import School, Tariff, TariffPlan
 from schools.school_mixin import SchoolMixin
 from users.models import UserGroup, UserPseudonym
@@ -322,7 +322,9 @@ class AccessDistributionView(
                 else:
                     user.groups.get(group=group, school=school).delete()
                     try:
-                        user_pseudonym = UserPseudonym.objects.get(user=user, school=school, pseudonym=pseudonym)
+                        user_pseudonym = UserPseudonym.objects.get(
+                            user=user, school=school, pseudonym=pseudonym
+                        )
                         user_pseudonym.delete()
                     except UserPseudonym.DoesNotExist:
                         pass
@@ -413,27 +415,41 @@ class AccessDistributionView(
         return HttpResponse("Доступ успешно заблокирован", status=201)
 
     @swagger_auto_schema(
-        request_body=openapi.Schema(
-            type=openapi.TYPE_OBJECT,
-            properties={
-                'user_ids': openapi.Schema(type=openapi.TYPE_ARRAY, items=openapi.Items(type=openapi.TYPE_INTEGER)),
-                'role': openapi.Schema(type=openapi.TYPE_STRING),
-                'new_group_id': openapi.Schema(type=openapi.TYPE_INTEGER),
-            },
-            required=['user_ids', 'role', 'new_group_id'],
-        ),
+        manual_parameters=[
+            openapi.Parameter(
+                "user_ids",
+                openapi.IN_FORM,
+                type=openapi.TYPE_ARRAY,
+                items=openapi.Items(type=openapi.TYPE_INTEGER),
+                required=True,
+            ),
+            openapi.Parameter(
+                "role", openapi.IN_FORM, type=openapi.TYPE_STRING, required=True
+            ),
+            openapi.Parameter(
+                "new_group_id",
+                openapi.IN_FORM,
+                type=openapi.TYPE_INTEGER,
+                required=True,
+            ),
+        ],
         tags=["access_distribution"],
         operation_description="Update user group",
         consumes=["multipart/form-data"],
     )
-    @action(detail=True, methods=['patch'], url_path='update-group', url_name='update-group')
+    @action(
+        detail=True, methods=["patch"], url_path="update-group", url_name="update-group"
+    )
     def update_group(self, request, *args, **kwargs):
-        id = kwargs.get('pk')
-        user_ids = request.data.get('user_ids')
-        new_group_id = request.data.get('new_group_id')
+        id = kwargs.get("pk")
+        user_ids = request.data.get("user_ids")
+        new_group_id = request.data.get("new_group_id")
 
         if id is None:
-            return Response({"detail": "Идентификатор не предоставлен"}, status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "Идентификатор не предоставлен"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         if user_ids:
             user_ids = json.loads(user_ids)
@@ -442,9 +458,9 @@ class AccessDistributionView(
             new_group_id = int(new_group_id)
 
         data = {
-            'user_ids': user_ids,
-            'role': request.data.get('role'),
-            'student_groups': [new_group_id] if new_group_id else []
+            "user_ids": user_ids,
+            "role": request.data.get("role"),
+            "student_groups": [new_group_id] if new_group_id else [],
         }
         serializer = AccessDistributionSerializer(data=data, partial=True)
 
@@ -452,28 +468,43 @@ class AccessDistributionView(
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
         user_ids = serializer.validated_data.get("user_ids")
-        new_group_id = serializer.validated_data.get("student_groups", [])[0] if serializer.validated_data.get("student_groups") else None
+        new_group_id = (
+            serializer.validated_data.get("student_groups", [])[0]
+            if serializer.validated_data.get("student_groups")
+            else None
+        )
 
         if not user_ids or new_group_id is None:
-            return Response({"detail": "user_ids и student_groups должны быть предоставлены"},
-                            status=status.HTTP_400_BAD_REQUEST)
+            return Response(
+                {"detail": "user_ids и student_groups должны быть предоставлены"},
+                status=status.HTTP_400_BAD_REQUEST,
+            )
 
         school = self.get_school()
 
         try:
-            new_group = StudentsGroup.objects.get(pk=new_group_id, course_id__school=school)
+            new_group = StudentsGroup.objects.get(
+                pk=new_group_id, course_id__school=school
+            )
         except StudentsGroup.DoesNotExist:
-            return Response({"detail": "Группа не найдена или не принадлежит вашей школе"},
-                            status=status.HTTP_404_NOT_FOUND)
+            return Response(
+                {"detail": "Группа не найдена или не принадлежит вашей школе"},
+                status=status.HTTP_404_NOT_FOUND,
+            )
 
         for user_id in user_ids:
             try:
                 user = User.objects.get(pk=user_id)
             except User.DoesNotExist:
-                return Response({"detail": f"Пользователь с id={user_id} не найден"}, status=status.HTTP_404_NOT_FOUND)
+                return Response(
+                    {"detail": f"Пользователь с id={user_id} не найден"},
+                    status=status.HTTP_404_NOT_FOUND,
+                )
 
             # Найти текущую группу пользователя в данном курсе
-            current_group = user.students_group_fk.filter(course_id__school=school).first()
+            current_group = user.students_group_fk.filter(
+                course_id__school=school
+            ).first()
 
             if current_group:
                 # Удалить пользователя из текущей группы
@@ -491,7 +522,9 @@ class AccessDistributionView(
                             teacher_chat.save()
 
             # Добавить пользователя в новую группу
-            if not user.students_group_fk.filter(course_id=new_group.course_id).exists():
+            if not user.students_group_fk.filter(
+                course_id=new_group.course_id
+            ).exists():
                 user.students_group_fk.add(new_group)
 
         return Response({"message": "Группа обновлена"}, status=status.HTTP_200_OK)
