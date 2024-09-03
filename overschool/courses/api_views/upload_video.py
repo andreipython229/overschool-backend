25f031c98e27b9311929b54bb36bc27efb141dc4
Kolesnikov Denis <denkoles3k@gmail.com>
2024-09-03 12:37:14 +0300

    add huey upload video

--- overschool/courses/api_views/upload_video.py
+++ overschool/courses/api_views/upload_video.py
@@ -1,3 +1,5 @@
+from io import BytesIO
+
 from common_services.mixins import LoggingMixin, WithHeadersViewSet
 from common_services.selectel_client import UploadToS3
 from courses.models.common.base_lesson import BaseLesson, BaseLessonBlock
@@ -66,7 +68,8 @@ class UploadVideoViewSet(
             base_lesson = BaseLesson.objects.get(pk=instance.base_lesson.id)
             # Отправляем задачу в Huey
             file_path = s3.file_path(video, base_lesson)
-            upload_video_task(video, file_path)
+            video_stream = BytesIO(video.read())
+            upload_video_task(video_stream, file_path)
             serializer.validated_data["video"] = file_path
         if picture:
             if instance.picture:
