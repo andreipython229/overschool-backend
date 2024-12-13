f2eae424fb62b72ba42a52f13fd01e9fff2f9465
Kolesnikov Denis <denkoles3k@gmail.com>
2024-12-13 13:51:37 +0300

    fix

--- overschool/schools/models/__init__.py
+++ overschool/schools/models/__init__.py
@@ -1,4 +1,3 @@
-from .box import Box, BoxPrize, Payment, Prize, UserPrize
 from .promocode import PromoCode
 from .school import (
     ProdamusPaymentLink,
@@ -22,3 +21,4 @@ from .school_referral import Referral, ReferralClick
 from .school_roles import SchoolNewRole
 from .schoolMeetings import SchoolMeetings
 from .userDomain import Domain
+from .box import Box, BoxPrize, Payment, Prize, UserPrize
